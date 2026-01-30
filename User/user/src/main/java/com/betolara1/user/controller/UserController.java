bd7cfbd9946e273aa17f3de33fa7e8f1cc4dca29
roberto <robertokantovitzlara@hotmail.com>
2026-01-30 13:49:25 -0300

    user completo

--- User/user/src/main/java/com/betolara1/user/controller/UserController.java
+++ User/user/src/main/java/com/betolara1/user/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.betolara1.user.controller;
 
 import java.util.Map;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -25,6 +26,15 @@ public class UserController {
         return ResponseEntity.ok(newUser);
     }
 
-    
+    @PostMapping("/login")
+    public ResponseEntity<?> loginUser(@RequestBody Map<String, String> request) {
+        Optional<User> user = userService.findByUsername(request.get("username"));
+
+        if (user.isPresent() && user.get().getPassword().equals(request.get("password"))) {
+            return ResponseEntity.ok(user);
+        } else {
+            return ResponseEntity.status(401).body("Invalid credentials");
+        }
+    }
 
 }
