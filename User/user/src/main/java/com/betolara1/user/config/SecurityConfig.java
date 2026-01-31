6c0845d63e83e62beb5caeb9dcada31536d1b479
roberto lara <robertokantovitzlara@hotmail.com>
2026-01-31 20:45:42 -0300

    configuração no arquivo SecurityConfig.java pra permitir qualquer usuario criar seu usuario

--- User/user/src/main/java/com/betolara1/user/config/SecurityConfig.java
+++ User/user/src/main/java/com/betolara1/user/config/SecurityConfig.java
@@ -2,14 +2,33 @@ package com.betolara1.user.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    // Este é o novo método que resolve o erro 401
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable) // Desabilita o CSRF para facilitar os testes no Insomnia
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers(HttpMethod.POST, "/auth/register").permitAll() // Libera o cadastro
+                .anyRequest().authenticated() // Bloqueia todo o resto
+            );
+
+        return http.build();
     }
 }
