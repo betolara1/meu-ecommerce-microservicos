b9c7f7ad33aab1ebe57722e31d29be078a43f89c
roberto <robertokantovitzlara@hotmail.com>
2026-03-05 17:28:04 -0300

    inserido o sistema de API Gateway para padronizar as portas de cada serviço

--- Inventory/inventory/src/main/java/com/betolara1/inventory/InventoryApplication.java
+++ Inventory/inventory/src/main/java/com/betolara1/inventory/InventoryApplication.java
@@ -3,9 +3,11 @@ package com.betolara1.inventory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 public class InventoryApplication {
 
 	public static void main(String[] args) {
